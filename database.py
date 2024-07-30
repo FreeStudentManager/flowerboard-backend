0f05e1c62e78a1e081313f81870c89c50b0996a7
XYCode Kerman <xycode-xyc@outlook.com>
2024-07-30 22:05:17 +0800

    chore(database.py): echo 随 DEBUG 参数开启

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlmodel import create_engine
 
-from config import DATABASE_URL
+from config import DATABASE_URL, DEBUG
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, echo=DEBUG)
 
 __all__ = ['engine']
